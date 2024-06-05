f755b8e4620d2d14914618584ab7cdbfe17b2510
Koushick <koushickmondal2021@gmail.com>
2024-06-05 11:29:34 +0530

    Add files via upload

---
+++ lv0_7.sql
@@ -0,0 +1,3 @@
+--Find the difference between the total number of CITY entries in the table and the number of distinct CITY entries in the table.
+select ((select count(CITY) as ans1 from STATION)-(select count(distinct CITY)
+       as ans2 from STATION));
