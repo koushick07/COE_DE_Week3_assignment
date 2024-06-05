0e0e042ba7772426adae988caf888c352d6cb534
Koushick <koushickmondal2021@gmail.com>
2024-06-05 11:29:49 +0530

    Add files via upload

---
+++ lv0_8.sql
@@ -0,0 +1,3 @@
+--Query the two cities in STATION with the shortest and longest CITY names, as well as their respective lengths (i.e.: number of characters in the name). If there is more than one smallest or largest city, choose the one that comes first when ordered alphabetically.
+select top 1 city, len(city) from station order by len(city) ASC, city ASC; 
+select top 1 city, len(city) from station order by len(city) DESC, city ASC;
